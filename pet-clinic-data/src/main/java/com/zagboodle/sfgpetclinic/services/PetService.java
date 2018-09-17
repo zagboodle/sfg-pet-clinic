2ac12d23d8bb34f01bf1c4843de802daa074a41d
Jeremy Jacobs <zagboodle@gmail.com>
2018-09-17 14:15:49 -0500

    Refactoring to common CRUD interface.  Closes #11.

--- pet-clinic-data/src/main/java/com/zagboodle/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/zagboodle/sfgpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package com.zagboodle.sfgpetclinic.services;
 
 import com.zagboodle.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/src/main/java/com/zagboodle/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/zagboodle/sfgpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package com.zagboodle.sfgpetclinic.services;
 
 import com.zagboodle.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
